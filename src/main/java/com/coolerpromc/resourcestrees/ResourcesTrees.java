abecb8fb4d7ed7c324e463a6c9dae4e96a98fdd5
CoolerProYT <veronlam1818@gmail.com>
2026-01-08 23:16:45 +0800

    1.5.0

--- src/main/java/com/coolerpromc/resourcestrees/ResourcesTrees.java
+++ src/main/java/com/coolerpromc/resourcestrees/ResourcesTrees.java
@@ -5,6 +5,7 @@ import com.coolerpromc.resourcestrees.block.custom.ResourcesLeavesBlock;
 import com.coolerpromc.resourcestrees.block.custom.ResourcesSaplingBlock;
 import com.coolerpromc.resourcestrees.block.entity.ModBlockEntities;
 import com.coolerpromc.resourcestrees.block.entity.custom.ResourcesTypesBlockEntity;
+import com.coolerpromc.resourcestrees.config.ModConfig;
 import com.coolerpromc.resourcestrees.core.ResourcesTypes;
 import com.coolerpromc.resourcestrees.datacomponent.ModDataComponents;
 import com.coolerpromc.resourcestrees.item.ModCreativeTab;
@@ -45,6 +46,7 @@ import java.util.List;
 public class ResourcesTrees implements ModInitializer {
 	public static final String MODID = "resourcestrees";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    public static ModConfig CONFIG = new ModConfig();
 
 	@Override
 	public void onInitialize() {
@@ -55,6 +57,8 @@ public class ResourcesTrees implements ModInitializer {
 		ModRecipes.register();
 		ModCreativeTab.register();
 		ModMenuTypes.register();
+
+        CONFIG.load();
 
 		Field[] fields = ModBlocks.class.getDeclaredFields();
 		for (Field field : fields){
--- src/main/java/com/coolerpromc/resourcestrees/item/custom/ModBlockItem.java
+++ src/main/java/com/coolerpromc/resourcestrees/item/custom/ModBlockItem.java
@@ -19,7 +19,7 @@ public class ModBlockItem extends BlockItem {
     public Text getName(ItemStack stack) {
         RegistryEntry<ResourcesTypes> type = stack.get(ModDataComponents.TYPE);
         if (type != null && (Block.getBlockFromItem(stack.getItem()) instanceof ResourcesSaplingBlock || Block.getBlockFromItem(stack.getItem()) instanceof ResourcesLeavesBlock)){
-            return Text.translatable(type.value().translationKey()).append(" ").append(super.getName(stack));
+            return Text.translatable("type.resourcestrees." + type.getKey().get().getValue().getPath()).append(" ").append(super.getName(stack));
         }
         return super.getName(stack);
     }
