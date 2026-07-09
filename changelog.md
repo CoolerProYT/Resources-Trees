4e64d22907803b1b562cb71ee29e609dfca04057
CoolerProYT <veronlam1818@gmail.com>
2026-07-09 15:14:53 +0800

    26.2.0.1

--- changelog.md
+++ changelog.md
@@ -1,1 +1,10 @@
-- Port to 26.2
+## 26.2.0.1
+### Changes
+- Tree Simulator now render sapling using block model instead of item model
+
+#### NeoForge
+- Added compat to Agritech Evolved
+- Added compat to Agritech Two
+
+#### Fabric
+- Added compat to Agritech
--- common/src/main/java/com/coolerpromc/resourcestrees/block/entity/renderstate/TreeSimulatorRenderState.java
+++ common/src/main/java/com/coolerpromc/resourcestrees/block/entity/renderstate/TreeSimulatorRenderState.java
@@ -1,10 +1,11 @@
 package com.coolerpromc.resourcestrees.block.entity.renderstate;
 
 import com.coolerpromc.resourcestrees.block.entity.custom.TreeSimulatorBlockEntity;
+import net.minecraft.client.renderer.block.BlockModelRenderState;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
 
 public class TreeSimulatorRenderState extends BlockEntityRenderState {
     public TreeSimulatorBlockEntity blockEntity;
-    public ItemStackRenderState itemStackRenderState;
+    public BlockModelRenderState blockModelRenderState;
 }
